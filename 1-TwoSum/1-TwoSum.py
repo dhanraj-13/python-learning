73c37327d86f3998e4616c4e20c65274de49d0d4
DHANRAJ R <dhan0474@gmail.com>
2026-07-14 13:57:32 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.py
@@ -0,0 +1,9 @@
+# Last updated: 7/14/2026, 1:57:31 PM
+class Solution(object):
+    def twoSum(self, nums, target):
+        num_map = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
