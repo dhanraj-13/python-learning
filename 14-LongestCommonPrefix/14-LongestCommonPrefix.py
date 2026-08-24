03e20e3b04cb5cd346ab77b5c3c20394ebbff98f
DHANRAJ R <dhan0474@gmail.com>
2026-08-24 15:52:46 +0530

    File updated at 8/24/2026, 3:52:50 PM

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.py
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.py
@@ -1,15 +1,32 @@
-# Last updated: 8/24/2026, 3:51:51 PM
-1class Solution(object):
-2    def searchInsert(self, nums, target):
-3        l = 0
-4        r = len(nums) - 1
-5        while l <= r:
-6            mid = (l + r) // 2
-7            if nums[mid] < target:
-8                l = mid + 1
-9            elif nums[mid] > target:
-10                r = mid - 1
-11            else:
-12                return mid
-13        return l
-14        
+# Last updated: 8/24/2026, 3:52:50 PM
+1class Solution:
+2    def isValidSudoku(self, board: List[List[str]]) -> bool:
+3        for row in range(0,9,3):
+4            for col in range(0,9,3):
+5                m=set()
+6                for i in range(row,row+3):
+7                    for j in range(col,col+3):
+8                        if board[i][j]=='.':
+9                            continue
+10                        if board[i][j] in m:
+11                            return False
+12                        m.add(board[i][j])
+13        for i in range(0,9):
+14            m=set()
+15            for j in range(0,9):
+16                if board[i][j]=='.':
+17                    continue
+18                if board[i][j] in m:
+19                    return False
+20                m.add(board[i][j])
+21        for i in range(0,9):
+22            m=set()
+23            for j in range(0,9):
+24                if board[j][i]=='.':
+25                    continue
+26                if board[j][i] in m:
+27                    return False
+28                m.add(board[j][i])
+29        return True
+30    
+31
