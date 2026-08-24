9e00f01289c575152c777f55106ac09ff5614130
DHANRAJ R <dhan0474@gmail.com>
2026-08-24 15:54:20 +0530

    File updated at 8/24/2026, 3:54:25 PM

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.py
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.py
@@ -1,32 +1,13 @@
-# Last updated: 8/24/2026, 3:52:50 PM
+# Last updated: 8/24/2026, 3:54:25 PM
 1class Solution:
-2    def isValidSudoku(self, board: List[List[str]]) -> bool:
-3        for row in range(0,9,3):
-4            for col in range(0,9,3):
-5                m=set()
-6                for i in range(row,row+3):
-7                    for j in range(col,col+3):
-8                        if board[i][j]=='.':
-9                            continue
-10                        if board[i][j] in m:
-11                            return False
-12                        m.add(board[i][j])
-13        for i in range(0,9):
-14            m=set()
-15            for j in range(0,9):
-16                if board[i][j]=='.':
-17                    continue
-18                if board[i][j] in m:
-19                    return False
-20                m.add(board[i][j])
-21        for i in range(0,9):
-22            m=set()
-23            for j in range(0,9):
-24                if board[j][i]=='.':
-25                    continue
-26                if board[j][i] in m:
-27                    return False
-28                m.add(board[j][i])
-29        return True
-30    
-31
+2    def plusOne(self, digits: List[int]) -> List[int]:
+3
+4        for i in range(len(digits)-1, -1, -1):
+5            if digits[i] == 9:
+6                digits[i] = 0
+7            else:
+8                digits[i] = digits[i] + 1
+9                return digits
+10        return [1] + digits 
+11
+12        
