e1362d37037a238089cfb984185a2ce8293a0d8a
DHANRAJ R <dhan0474@gmail.com>
2026-08-26 22:53:59 +0530

    File updated at 8/26/2026, 10:53:59 PM

--- 179-LargestNumber/179-LargestNumber.py
+++ 179-LargestNumber/179-LargestNumber.py
@@ -1,15 +1,7 @@
-# Last updated: 8/26/2026, 10:44:28 PM
-1class Solution:
-2    def maxProduct(self, words: List[str]) -> int:
-3        #Solution2 (Optimal)
-4        ans = 0
-5        n = len(words)
-6        binary_rep = [0]*n
-7        for i in range(n):
-8            curr_word = words[i]
-9            for letter in curr_word:
-10                binary_rep[i] = binary_rep[i] | (1<<(ord(letter)-ord('a')))
-11            for j in range(i):
-12                if binary_rep[i] & binary_rep[j] == 0:
-13                    ans = max(ans, len(words[i])*len(words[j]))
-14        return ans
+# Last updated: 8/26/2026, 10:53:59 PM
+1class Solution(object):
+2    def thirdMax(self, nums):
+3        s=sorted(list(set(nums)),reverse=True)
+4        if len(s)<3:
+5            return max(s)
+6        return s[2]
