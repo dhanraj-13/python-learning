378a4ef9be29b4e73e7063a996a861c8427e9814
DHANRAJ R <dhan0474@gmail.com>
2026-08-26 23:29:20 +0530

    File updated at 8/26/2026, 11:29:19 PM

--- 179-LargestNumber/179-LargestNumber.py
+++ 179-LargestNumber/179-LargestNumber.py
@@ -1,17 +1,15 @@
-# Last updated: 8/26/2026, 11:13:43 PM
+# Last updated: 8/26/2026, 11:29:19 PM
 1class Solution:
-2    def findRelativeRanks(self, scores: List[int]) -> List[str]:
-3        heap = []
-4        for i, score in enumerate(scores):
-5            heapq.heappush(heap, (-score, i))
-6
-7        ranks = {0: "Gold Medal", 1: "Silver Medal", 2: "Bronze Medal"}
-8
-9        result = [0] * len(scores)
-10
-11        for rank in range(len(scores)):
-12            _, idx = heapq.heappop(heap)
-13            result[idx] = ranks.get(rank, str(rank + 1))
-14
-15
-16        return result 
+2    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+3        words1 = {word: idx for idx, word in enumerate(list1)}
+4
+5        min_sum = math.inf
+6        for idx2, word2 in enumerate(list2):
+7            if word2 in words1:
+8                if words1[word2] + idx2 < min_sum:
+9                    min_sum = words1[word2] + idx2
+10                    min_words = [word2]
+11                elif words1[word2] + idx2 == min_sum:
+12                    min_words.append(word2)
+13                   
+14        return min_words
