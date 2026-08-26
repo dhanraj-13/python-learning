a5a0a6e54bf0dc750279e450e1d4112227e366f2
DHANRAJ R <dhan0474@gmail.com>
2026-08-26 22:44:28 +0530

    File updated at 8/26/2026, 10:44:28 PM

--- 179-LargestNumber/179-LargestNumber.py
+++ 179-LargestNumber/179-LargestNumber.py
@@ -1,9 +1,15 @@
-# Last updated: 8/26/2026, 10:29:15 PM
-1class Solution(object):
-2    def largestNumber(self, nums):
-3        a = [str(i) for i in nums]
-4        a.sort(cmp=lambda x, y: -1 if x + y > y + x else (1 if x + y < y + x else 0))
-5        s = "".join(a)
-6        while len(s) > 1 and s[0] == '0':
-7            s = s[1:]
-8        return s
+# Last updated: 8/26/2026, 10:44:28 PM
+1class Solution:
+2    def maxProduct(self, words: List[str]) -> int:
+3        #Solution2 (Optimal)
+4        ans = 0
+5        n = len(words)
+6        binary_rep = [0]*n
+7        for i in range(n):
+8            curr_word = words[i]
+9            for letter in curr_word:
+10                binary_rep[i] = binary_rep[i] | (1<<(ord(letter)-ord('a')))
+11            for j in range(i):
+12                if binary_rep[i] & binary_rep[j] == 0:
+13                    ans = max(ans, len(words[i])*len(words[j]))
+14        return ans
