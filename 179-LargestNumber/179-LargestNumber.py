da6f5b90ea850c31f1a96df3011d49dbda2cbe48
DHANRAJ R <dhan0474@gmail.com>
2026-08-26 23:00:00 +0530

    File updated at 8/26/2026, 11:00:00 PM

--- 179-LargestNumber/179-LargestNumber.py
+++ 179-LargestNumber/179-LargestNumber.py
@@ -1,7 +1,15 @@
-# Last updated: 8/26/2026, 10:56:08 PM
-1class Solution(object):
-2    def thirdMax(self, nums):
-3        s=sorted(list(set(nums)),reverse=True)
-4        if len(s)<3:
-5            return max(s)
-6        return s[2]
+# Last updated: 8/26/2026, 11:00:00 PM
+1class Solution:
+2    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+3        g.sort()
+4        s.sort()
+5        count = 0
+6        i = 0
+7        j = 0
+8        while i < len(g) and j< len(s):
+9            if s[j] >= g[i]:
+10                count+=1
+11                i+=1 
+12            j+=1
+13        return count
+14        
