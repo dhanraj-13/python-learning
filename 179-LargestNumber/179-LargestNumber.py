5754c27a74fb843c276d81a573e453e85d03bf0f
DHANRAJ R <dhan0474@gmail.com>
2026-08-26 23:08:25 +0530

    File updated at 8/26/2026, 11:08:25 PM

--- 179-LargestNumber/179-LargestNumber.py
+++ 179-LargestNumber/179-LargestNumber.py
@@ -1,15 +1,10 @@
-# Last updated: 8/26/2026, 11:00:00 PM
-1class Solution:
-2    def findContentChildren(self, g: List[int], s: List[int]) -> int:
-3        g.sort()
-4        s.sort()
-5        count = 0
-6        i = 0
-7        j = 0
-8        while i < len(g) and j< len(s):
-9            if s[j] >= g[i]:
-10                count+=1
-11                i+=1 
-12            j+=1
-13        return count
-14        
+# Last updated: 8/26/2026, 11:08:25 PM
+1rows = (set("qwertyuiop"), set("asdfghjkl"), set("zxcvbnm"))
+2
+3
+4
+5class Solution:
+6    def findWords(self, words: list[str]) -> list[str]:
+7        
+8        
+9        return [w for w in words if any(set(w.lower()).issubset(row) for row in rows)]
