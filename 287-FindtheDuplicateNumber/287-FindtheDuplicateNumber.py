629859f3ca4e66cf7b9aa071ebc0eeb13225bb61
DHANRAJ R <dhan0474@gmail.com>
2026-09-01 16:44:03 +0530

    File updated at 9/1/2026, 4:44:02 PM

--- 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.py
+++ 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.py
@@ -1,8 +1,9 @@
-# Last updated: 9/1/2026, 3:58:25 PM
+# Last updated: 9/1/2026, 4:44:02 PM
 1class Solution:
-2    def findDuplicate(self, nums: List[int]) -> int:
-3        seen = set()
+2    def singleNumber(self, nums: List[int]) -> int:
+3        result = 0
 4        for num in nums:
-5            if num in seen:
-6                return num
-7            seen.add(num)
+5            result = result ^ num
+6        return result
+7
+8        #using xor ^
