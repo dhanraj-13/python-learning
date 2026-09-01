91ef416a8e5628d205bfd1294f03737afbc1d9a4
DHANRAJ R <dhan0474@gmail.com>
2026-09-01 15:58:26 +0530

    File updated at 9/1/2026, 3:58:25 PM

--- 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.py
+++ 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.py
@@ -1,10 +1,8 @@
-# Last updated: 9/1/2026, 1:46:03 PM
+# Last updated: 9/1/2026, 3:58:25 PM
 1class Solution:
-2    def containsDuplicate(self, nums: List[int]) -> bool:
+2    def findDuplicate(self, nums: List[int]) -> int:
 3        seen = set()
 4        for num in nums:
 5            if num in seen:
-6                return True
-7            else:
-8                seen.add(num)
-9        return False
+6                return num
+7            seen.add(num)
