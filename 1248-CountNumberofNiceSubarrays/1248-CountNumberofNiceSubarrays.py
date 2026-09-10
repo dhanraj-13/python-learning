f6b0bcfc772832b2059caa2c7d5f3cb88a64b082
DHANRAJ R <dhan0474@gmail.com>
2026-09-10 13:41:40 +0530

    Create 1248-CountNumberofNiceSubarrays

---
+++ 1248-CountNumberofNiceSubarrays/1248-CountNumberofNiceSubarrays.py
@@ -0,0 +1,16 @@
+# Last updated: 9/10/2026, 1:41:39 PM
+1class Solution:
+2    def numberOfSubarrays(self, nums: list[int], k: int) -> int:
+3        n = len(nums)
+4        
+5        odds = [-1] + [i for i, num in enumerate(nums) if num % 2 != 0] + [n]
+6        
+7        total_subarrays = 0
+8        
+9        for i in range(1, len(odds) - k):
+10            left_choices = odds[i] - odds[i - 1]
+11            right_choices = odds[i + k] - odds[i + k - 1]
+12            
+13            total_subarrays += left_choices * right_choices
+14            
+15        return total_subarrays
