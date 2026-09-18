02b2bed74c206910b2d66b37a9f05933448f7b0e
DHANRAJ R <dhan0474@gmail.com>
2026-09-18 12:02:39 +0530

    File updated at 9/18/2026, 12:02:38 PM

--- 457-CircularArrayLoop/457-CircularArrayLoop.py
+++ 457-CircularArrayLoop/457-CircularArrayLoop.py
@@ -1,36 +1,22 @@
-# Last updated: 9/18/2026, 11:56:58 AM
+# Last updated: 9/18/2026, 12:02:38 PM
 1class Solution:
-2    def circularArrayLoop(self, nums: List[int]) -> bool:
-3        n = len(nums)
-4
-5        def nxt(i):
-6            return (i + nums[i]) % n
-7
-8        for i in range(n):
-9
-10            sp = i
-11            fp = i
-12            curr_dir = nums[i] > 0
-13
-14            while True:
-15
-16                if (nums[sp] > 0) != curr_dir:
-17                    break
-18
-19                if (nums[fp] > 0) != curr_dir:
-20                    break
-21
-22                if (nums[nxt(fp)] > 0) != curr_dir:
-23                    break
-24
-25                sp = nxt(sp)
-26                fp = nxt(nxt(fp))
-27
-28                if sp == fp:
-29
-30                    if sp == nxt(sp):
-31                        break
-32
-33                    return True
-34
-35        return False
+2    def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
+3        self.ans = ""
+4        self.dfs(root, "")
+5        return self.ans
+6
+7    def dfs(self, root, current_string):
+8        # If the current node is none, return
+9        if not root:
+10            return
+11
+12      
+13        current_string = chr(root.val + ord('a')) + current_string
+14
+15        if not root.left and not root.right:
+16           
+17            if not self.ans or self.ans > current_string:
+18                self.ans = current_string
+19        
+20        self.dfs(root.left, current_string)
+21        self.dfs(root.right, current_string)
