262ef39febabb68576957db1f2cac7ba85de570e
DHANRAJ R <dhan0474@gmail.com>
2026-09-18 12:41:16 +0530

    File updated at 9/18/2026, 12:41:15 PM

--- 457-CircularArrayLoop/457-CircularArrayLoop.py
+++ 457-CircularArrayLoop/457-CircularArrayLoop.py
@@ -1,25 +1,14 @@
-# Last updated: 9/18/2026, 12:28:56 PM
+# Last updated: 9/18/2026, 12:41:15 PM
 1class Solution:
-2    def isPossible(self, nums: List[int]) -> bool:
-3        subsequences = []
-4        
-5        for num in nums:
-6            while subsequences and subsequences[0][0] + 1 < num:
-7                sub = heapq.heappop(subsequences)
-8                if sub[1] < 3:
-9                    return False
-10            
-11            if not subsequences or subsequences[0][0] == num:
-12                heapq.heappush(subsequences, [num, 1])
-13            else:
-14                sub = heapq.heappop(subsequences)
-15                sub[0] += 1
-16                sub[1] += 1
-17                heapq.heappush(subsequences, sub)
-18                
-19        while subsequences:
-20            sub = heapq.heappop(subsequences)
-21            if sub[1] < 3:
-22                return False
-23            
-24        return True
+2    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+3        strs_table = {}
+4
+5        for string in strs:
+6            sorted_string = ''.join(sorted(string))
+7
+8            if sorted_string not in strs_table:
+9                strs_table[sorted_string] = []
+10
+11            strs_table[sorted_string].append(string)
+12
+13        return list(strs_table.values())
