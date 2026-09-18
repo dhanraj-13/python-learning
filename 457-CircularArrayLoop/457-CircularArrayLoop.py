dd34164b549f3944587a7fb68f0242d4aa193463
DHANRAJ R <dhan0474@gmail.com>
2026-09-18 12:28:56 +0530

    File updated at 9/18/2026, 12:28:56 PM

--- 457-CircularArrayLoop/457-CircularArrayLoop.py
+++ 457-CircularArrayLoop/457-CircularArrayLoop.py
@@ -1,22 +1,25 @@
-# Last updated: 9/18/2026, 12:02:38 PM
+# Last updated: 9/18/2026, 12:28:56 PM
 1class Solution:
-2    def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
-3        self.ans = ""
-4        self.dfs(root, "")
-5        return self.ans
-6
-7    def dfs(self, root, current_string):
-8        # If the current node is none, return
-9        if not root:
-10            return
-11
-12      
-13        current_string = chr(root.val + ord('a')) + current_string
-14
-15        if not root.left and not root.right:
-16           
-17            if not self.ans or self.ans > current_string:
-18                self.ans = current_string
-19        
-20        self.dfs(root.left, current_string)
-21        self.dfs(root.right, current_string)
+2    def isPossible(self, nums: List[int]) -> bool:
+3        subsequences = []
+4        
+5        for num in nums:
+6            while subsequences and subsequences[0][0] + 1 < num:
+7                sub = heapq.heappop(subsequences)
+8                if sub[1] < 3:
+9                    return False
+10            
+11            if not subsequences or subsequences[0][0] == num:
+12                heapq.heappush(subsequences, [num, 1])
+13            else:
+14                sub = heapq.heappop(subsequences)
+15                sub[0] += 1
+16                sub[1] += 1
+17                heapq.heappush(subsequences, sub)
+18                
+19        while subsequences:
+20            sub = heapq.heappop(subsequences)
+21            if sub[1] < 3:
+22                return False
+23            
+24        return True
